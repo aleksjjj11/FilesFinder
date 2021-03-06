82c1ca91bfd9adaabfc496cce921d8296eb53183
Алексеев Алексей <Alale20002504@gmail.com>
2021-03-06 18:26:52 +0500

    Added unit tests

---
+++ TestOfLogic/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using FilesFinder.ViewModels;
+using NUnit.Framework;
+
+namespace TestOfLogic
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase("C:\\Users\\DNS\\YandexDisk\\WorkUnik\\TRPO\\Test", "*.*", "doc1.txt; *.c")]
+        public void GetFilesTest(string currentDirectory, string findMask, string excludeMask)
+        {
+            var viewmodel = new MainViewModel
+            {
+                CurrentDirectory = currentDirectory,
+                FileMask = findMask,
+                ExcludeMask = excludeMask,
+            };
+            var res = viewmodel.GetFilesEnumerable().ToArray();
+            Assert.Pass();
+        }
+    }
+}
